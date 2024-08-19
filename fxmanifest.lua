69b239fddc964bc85c50167b2ae0110720543cf2
zaid12z <129253512+zaid12z@users.noreply.github.com>
2024-08-18 18:57:17 -0700

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,29 @@
+fx_version 'cerulean'
+
+game "gta5"
+
+author "Rexx"
+version '2.5.5'
+
+lua54 'yes'
+
+ui_page 'web/index.html'
+
+shared_script {
+  "config.lua",
+  '@ox_lib/init.lua',
+}
+
+server_script {
+  "server/**",
+  '@oxmysql/lib/MySQL.lua',
+}
+
+client_script {
+  'client/**',
+}
+
+files {
+  'web/**',
+  "node_modules/fivem-game-view/**/*",
+}
